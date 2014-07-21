3dd8dc7466819e31c085016978f0c86f43bc95e4
Ribesg <ribesg@yahoo.fr>
2014-07-21 11:53:56 +0200

    The i parameter is _always_ 1 for End portals

--- src/main/java/net/minecraft/server/Entity.java
+++ src/main/java/net/minecraft/server/Entity.java
@@ -8,7 +8,6 @@ import java.util.concurrent.Callable;
 // CraftBukkit start
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.TravelAgent;
 import org.bukkit.block.BlockFace;
@@ -1785,24 +1784,7 @@ public abstract class Entity {
             boolean useTravelAgent = exitWorld != null && !(this.dimension == 1 && exitWorld.dimension == 1); // don't use agent for custom worlds or return from THE_END
 
             TravelAgent agent = exit != null ? (TravelAgent) ((CraftWorld) exit.getWorld()).getHandle().getTravelAgent() : org.bukkit.craftbukkit.CraftTravelAgent.DEFAULT; // return arbitrary TA to compensate for implementation dependent plugins
-            TeleportCause cause = enter.getBlock().getType() == org.bukkit.Material.PORTAL ? TeleportCause.NETHER_PORTAL : TeleportCause.END_PORTAL;
-            if (enter.getBlock().getType() == Material.ENDER_PORTAL) {
-                cause = TeleportCause.END_PORTAL;
-            } else if (enter.getBlock().getType() == Material.PORTAL) {
-                cause = TeleportCause.NETHER_PORTAL;
-            } else {
-                java.util.Set<Material> relativeMaterials = new java.util.HashSet<Material>(BlockFace.values().length);
-                for (BlockFace direction : BlockFace.values()) {
-                    relativeMaterials.add(enter.getBlock().getRelative(direction).getType());
-                }
-                if (relativeMaterials.contains(Material.ENDER_PORTAL)) {
-                    cause = TeleportCause.END_PORTAL;
-                } else if (relativeMaterials.contains(Material.PORTAL)) {
-                    cause = TeleportCause.NETHER_PORTAL;
-                } else {
-                    // TODO ?
-                }
-            }
+            TeleportCause cause = i == 1 ? TeleportCause.END_PORTAL : TeleportCause.NETHER_PORTAL;
             EntityPortalEvent event = new EntityPortalEvent(this.getBukkitEntity(), enter, exit, agent, cause);
             event.useTravelAgent(useTravelAgent);
             event.getEntity().getServer().getPluginManager().callEvent(event);
